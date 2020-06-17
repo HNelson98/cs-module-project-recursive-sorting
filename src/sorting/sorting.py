17ee4a5484683b410c74502c8c96c44c3d2761fb
Henry Nelson <Henry.Nelson89@gmail.com>
2020-06-17 12:45:48 -0700

    Okay i guess i didn't do this before

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -4,6 +4,21 @@ def merge(arrA, arrB):
     merged_arr = [0] * elements
 
     # Your code here
+    a, b = 0, 0
+
+    for i in range(len(merged_arr)):
+        if a>= len(arrA):
+            merged_arr[i] = arrB[b]
+            b +=1
+        elif b >= len(arrB):
+            merged_arr[i] = arrA[a]
+            a +=1
+        elif arrA[a] < arrB[b]:
+            merged_arr[i] = arrA[a]
+            a +=1
+        else:
+            merged_arr[i] = arrB[b]
+            b +=1
 
 
     return merged_arr
@@ -19,10 +34,10 @@ def merge_sort(arr):
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
 # or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
-    # Your code here
+# def merge_in_place(arr, start, mid, end):
+#     # Your code here
 
 
-def merge_sort_in_place(arr, l, r):
-    # Your code here
+# def merge_sort_in_place(arr, l, r):
+#     # Your code here
 
